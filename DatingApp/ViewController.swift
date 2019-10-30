07cd4ab62be1eb9a3f7ec02e4d7123d56f1069ed
Funataku <funakoshi.t@yahoo.com>
2019-10-30 15:29:22 +0900

    Initial Commit

---
+++ DatingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatingApp
+//
+//  Created by Taku Funakoshi on 2019/10/30.
+//  Copyright © 2019 Taku Funakoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DatingAppTests/DatingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DatingAppTests.swift
+//  DatingAppTests
+//
+//  Created by Taku Funakoshi on 2019/10/30.
+//  Copyright © 2019 Taku Funakoshi. All rights reserved.
+//
+
+import XCTest
+@testable import DatingApp
+
+class DatingAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
